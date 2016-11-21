660b39a355bf150d78aa1e7f9ab2bd9d90c6f0ab
Diogo RBG <diogorbg@gmail.com>
2016-11-21 12:51:53 -0200

    movement_1

---
+++ Examples-Unity5/Assets/01-movement_1/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace movement_1 {
+
+public class Player : MonoBehaviour {
+
+	public float velocity = 5f;
+
+	private Rigidbody rigb;
+
+	// Use this for initialization
+	void Start () {
+		// Obtains the physics component
+		rigb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Receive keyboard/joypad movement
+		Vector3 mov = Vector2.zero;
+		mov.x = Input.GetAxis("Horizontal");
+		mov.z = Input.GetAxis("Vertical");
+		mov.Normalize();
+
+		// Do not use transform.position to move ... use rigidbody.velocity
+		rigb.velocity = mov * velocity;
+
+		// Motion test
+		if (mov.magnitude >= 0.01f) {
+			transform.LookAt(transform.position + mov);
+		}
+	}
+}
+
+}
