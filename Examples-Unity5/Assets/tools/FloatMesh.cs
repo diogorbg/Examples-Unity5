4e5f2cf5a2a58c32300f545578212601bad84ef0
Diogo RBG <diogorbg@gmail.com>
2016-12-01 14:32:02 -0200

    mais exemplos

---
+++ Examples-Unity5/Assets/tools/FloatMesh.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter),typeof(MeshRenderer))]
+public class FloatMesh : MonoBehaviour {
+	//public enum Eixo { up, forward, rigth }
+
+	[HideInInspector] public Mesh original;
+	//public Eixo eixo;
+
+	private MeshFilter _meshFilter;
+	public Mesh sharedMesh {
+		get {
+			if (_meshFilter == null)
+				_meshFilter = GetComponent<MeshFilter>();
+			return _meshFilter.sharedMesh;
+		}
+		set {
+			if (_meshFilter == null)
+				_meshFilter = GetComponent<MeshFilter>();
+			_meshFilter.sharedMesh = value;
+		}
+	}
+
+	[ContextMenu("Float Mesh")]
+	void floatMesh () {
+		if (original==null)
+			original = sharedMesh;
+
+		Mesh mesh = new Mesh();
+		Vector3[] vertices = original.vertices;
+		Vector3[] normais = original.normals;
+		float z;
+		for(int i=0; i<vertices.Length; i++) {
+			z = -vertices[i].y;
+			vertices[i].y = vertices[i].z;
+			vertices[i].z = z;
+			z = -normais[i].y;
+			normais[i].y = normais[i].z;
+			normais[i].z = z;
+		}
+		mesh.vertices = vertices;
+		mesh.normals = normais;
+		mesh.subMeshCount = original.subMeshCount;
+		for (int i=0; i<original.subMeshCount; i++) {
+			mesh.SetTriangles(original.GetTriangles(i), i);
+		}
+		mesh.uv = original.uv;
+		this.sharedMesh = mesh;
+	}
+}
